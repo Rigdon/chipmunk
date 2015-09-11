229c983264da6635ffb5ecf2e2be8d1e7eb05732
Ryan Rigdon <ryan@thegroundwork.com>
2015-09-11 14:45:59 -0400

    Cleaning up before pypi upload.

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
+test_requirements = [
+    'mock',
+    'nose',
+]
+
 setup(
-  name='chipmunk',
-  version='1.0rc1',
-  description='A very small and simple usage mechanism for Python threadlocals.',
-  url='https://github.com/Rigdon/chipmunk',
-  author='Rigdon',
-  author_email='mr.rigdon@gmail.com',
-  license='The Unlicense',
-  packages=['chipmunk'],
-  zip_safe=False
+    name='chipmunk',
+    version='1.0rc1',
+    description='A very small and simple usage mechanism for Python threadlocals.',
+    url='https://github.com/Rigdon/chipmunk',
+    download_url='https://github.com/Rigdon/chipmunk/tarball/1.0rc1',
+    tests_require=test_requirements,
+    test_suite="nose.collector",
+    author='Rigdon',
+    author_email='mr.rigdon@gmail.com',
+    license='The Unlicense',
+    packages=['chipmunk'],
+    keywords=['utilities', 'locals'],
+    classifiers=[]
 )
