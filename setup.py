d1a3e329a681d11c24bb1da5e53a1d2712379c1f
Ryan Rigdon <ryan@thegroundwork.com>
2015-09-13 11:48:02 -0400

    Minor changes to setup files.

--- setup.py
+++ setup.py
@@ -5,6 +5,30 @@ test_requirements = [
     'nose',
 ]
 
+classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 5 - Production/Stable',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+
+    # License
+    "License :: Public Domain",
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+]
+
 setup(
     name='chipmunk',
     version='1.0',
@@ -17,6 +41,6 @@ setup(
     author_email='mr.rigdon@gmail.com',
     license='The Unlicense',
     packages=['chipmunk'],
-    keywords=['utilities', 'locals'],
+    keywords=['utilities', 'locals', 'threading'],
     classifiers=[]
 )
